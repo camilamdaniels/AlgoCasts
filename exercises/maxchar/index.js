ec1389dd64aef2ae0d3075d11322b6b7ea4c7349
Camila Daniels <camila.daniels926@gmail.com>
2018-08-14 12:56:36 -0500

    Max char solved.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -2,9 +2,30 @@
 // Given a string, return the character that is most
 // commonly used in the string.
 // --- Examples
-// maxChar("abcccccccd") === "c"
-// maxChar("apple 1231111") === "1"
+// console.log(maxChar("abcccccccd"));
+// console.log(maxChar("apple 1231111"));
 
-function maxChar(str) {}
+function maxChar(str) {
+	let obj = {};
+	for (let char of str) {
+		if (!obj.hasOwnProperty(char)) obj[char] = 1;
+		else obj[char] = obj[char]+=1;
+	}
+
+	// console.log(obj);
+
+	let keys = Object.keys(obj);
+	let max = 0;
+	let maxChar;
+
+	for (let key of keys) {
+		if (obj[key] > max) {
+			max = obj[key]; 
+			maxChar = key;
+		}
+	}
+
+	return maxChar;
+}
 
 module.exports = maxChar;
