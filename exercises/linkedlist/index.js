4047ac9b53a24be6ea2d07999273a9468d5c4f1a
Camila Daniels <camila.daniels926@gmail.com>
2018-09-13 10:58:37 -0500

    Implemented first 3 methods of linkedList class.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,39 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
 
-class LinkedList {}
+class LinkedList {
+	constructor() {
+		this.head = null;
+	}
+
+	insertFirst(data) {
+		if (!this.head) this.head = new Node(data);
+		else {
+			let newHead = new Node(data, this.head);
+			this.head = newHead;
+		}
+	}
+
+	size() {
+		let currNode = this.head;
+		let count = 0;
+		while (currNode) {
+			count++;
+			currNode = currNode.next;
+		}
+		return count;
+	}
+
+	getFirst() {
+		return this.head;
+	}
+}
 
 module.exports = { Node, LinkedList };
