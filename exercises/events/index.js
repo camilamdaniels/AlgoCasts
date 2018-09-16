55812006306f7a0fc45f5c8efc6e07676453162b
Camila Daniels <camila.daniels926@gmail.com>
2018-09-16 15:46:42 -0500

    Implemented Events class methods in exercises/events/index.js

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,35 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+  	this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+  	if (!this.events.hasOwnProperty(eventName)) {
+  		this.events[eventName] = [callback];
+  	} 
+  	else this.events[eventName].push(callback);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+  	if (this.events.hasOwnProperty(eventName)) {
+  		let cbs = this.events[eventName];
+  		for (let i = 0; i < cbs.length; i++) {
+  			cbs[i]();
+  		}
+  	}
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+  	if (this.events.hasOwnProperty(eventName)) {
+  		delete this.events[eventName];
+  	}
+  }
 }
 
 module.exports = Events;
