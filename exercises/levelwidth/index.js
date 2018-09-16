cc7a8c08c555154c7f03086b9c7c26bae8a939f1
Camila Daniels <camila.daniels926@gmail.com>
2018-09-16 14:05:15 -0500

    Level widths solved.

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	let queue = [root, null];
+	let widths = [0];
+	while (queue.length > 1) {
+		let node = queue.shift();
+		if (!node) {
+			widths.push(0);
+			queue.push(null);
+		} else {
+			queue.push(...node.children);
+			widths[widths.length - 1]++;
+		}
+	}
+	return widths;
+}
 
 module.exports = levelWidth;
